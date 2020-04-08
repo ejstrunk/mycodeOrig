e0a06dd760ba9bd5f1257d6270ae1334d48ce3ec
Everett Strunk <combattxwombatt@gmail.com>
2020-04-08 13:35:30 -0400

    Looping techniques

---
+++ fact/looping02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# open file in read mode
+dnsfile = open("dnsservers.txt", "r")
+# create list of lines
+dnslist = dnsfile.readlines()
+# loop over lines
+for svr in dnslist:
+    #print and end without a newline
+    print(svr, end="")
+# close your file
+dnsfile.close()
---
+++ fact/looping02c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# open file in read mode
+with open("dnsservers.txt", "r") as dnsfile:
+    # indent to keep the dnsfile object open
+    # loop across the lines in the file
+    for svr in dnsfile:
+        #print and end without a newline
+        print(svr, end="")
+# no need to close our file - closed automatically
