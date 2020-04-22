6120f33bb8a761374b99b8d35c17095a51ad44d5
Everett Strunk <combattxwombatt@gmail.com>
2020-04-21 23:39:53 -0400

    started capstone project

---
+++ capstone/project.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from time import sleep
+import random
+import csv
+""" Skyrim Alchemy Index """
+
+# resources
+
+# sets up the csv as a dictionary
+def csv_setup:
+    
+
+def main():
+    print("Welcome to the Skyrim Alchemy Index, a convenient one-stop shop for all of your alchemical inquiries in the land of Tamriel")
+    # Needs window dressing!
+    
+
+
+def random():
