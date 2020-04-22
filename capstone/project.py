707844c620ccb060a4c05ec95a97ea94284fd97e
Everett Strunk <combattxwombatt@gmail.com>
2020-04-22 14:26:05 -0400

    working draft of Skyrim Alchemy Project

--- capstone/project.py
+++ capstone/project.py
@@ -22,8 +22,17 @@ print("""
     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n""")
 
-# bringing in the csv
+# bringing in the csv and effects txt
 ingredients_csv = pe.get_sheet(file_name="ingredients.csv")
+temp = open('pos_effects.txt', 'r')
+pos_effects = temp.readlines()
+pos_effects = [pos_effects[i][:-1] for i in range(len(pos_effects) - 1)] + [pos_effects[-1]]
+temp.close()
+
+# view all effects
+def view_effects():
+    for effect in pos_effects:
+        print(effect)
 
 # view all ingredients in detail
 def view_all():
@@ -64,7 +73,7 @@ def view_value():
     for item in ingredients_csv:
         print("%s: %s" % (item[0], item[7]))
 
-# define the function for viewing all ingredients by certain methods
+# function for viewing all ingredients by certain methods
 def options():
     choice = " "
     again = " "
@@ -102,9 +111,9 @@ def options():
         again = input("Continue this method of research, return to scouring the book, or restart?\n>").lower()
         if again == "q":
             sys.exit("Had a Blast! See You Next Time!")
-        elif again in ["research", "this method", "this method of research"]:
+        elif again in ["research", "this method", "this method of research", "continue this method of research"]:
             options()
-        elif again in ["book", "scour the book"]:
+        elif again in ["book", "scour the book", "return to the book", "return to scouring the book"]:
             book()
         elif again == "restart":
             main()
@@ -117,27 +126,52 @@ def book():  # function for basic lists of ingredients and effects
         init_choice = input("Would you like to view all ingredients or search by effect?\n>").lower()
         if init_choice == "q":
             sys.exit("See you later!")
-        elif init_choice in ["all", "view all"]:
+        elif init_choice in ["all", "view all", "ingredients", "view all ingredients"]:
             options()
-        elif init_choice == "effect":
-            eff = input('In what effect are you interested?\n>').title()
-            eff_rows = []
-            for i in range(1,len(ingredients_csv)):
-                if eff in ingredients_csv.row[i]:
-                    eff_rows.append(i)
-            eff_rows = list(set(eff_rows))
-            for i in eff_rows:
-                print('\n' + ingredients_csv.row[i][0] + '\n')
-            again = input("Would you like to continue?\n>").lower()
-            if again in ["yes", "y","yep", "yup"]:
-                book()
-            else:
-                sys.exit("Catch you next time!")
+        elif init_choice in ["effect", "effects", "search by effect"]:
+            eff = " "
+            while eff != "Q":
+                eff = input('In what effect are you interested?\n>').title()
+                if eff == "Q":
+                    sys.exit("Have a Great Day!")
+                else:
+                    if eff in pos_effects:
+                        print(f"Ingredients containing the {eff} effect are as follows\n======================================================\n")
+                        eff_rows = []
+                        for i in range(1,len(ingredients_csv)):
+                            if eff in ingredients_csv.row[i]:
+                                eff_rows.append(i)
+                        eff_rows = list(set(eff_rows))
+                        for i in eff_rows:
+                            print('\n' + ingredients_csv.row[i][0] + '\n')
+                        again = input("\nWould you like to continue?\n>").lower()
+                        if again in ["yes", "y","yep", "yup", "yah"]:
+                            book()
+                        else:
+                            sys.exit("Catch you next time!")
+                    else:
+                        print("\n" + eff + " is not a valid effect\n")
+                        eff_list = input("If you would like to view the list of possible effects, please type 'list'. Otherwise, press ENTER\n>").lower()
+                        if eff_list == "list":
+                            #for effect in pos_effects:
+                                #print(effect)
+                            view_effects()
+                        else:
+                            print("\nLet's try again!\n")
         else:
-            print("That is not a valid option. Please enter 'all' or 'effect.\n\nYou may also exit with 'q'")
-            continue
+            print("\nThat is not a valid option. Please enter 'all' or 'effect.\n\nYou may also exit with 'q'\n")
 
 def main():
-    start = input("Where would you like to begin?\nYou may retrieve the Guide Book or begin brewing on your own!\nAlso, feel free to take a break from your alchemical pursuits anytime with 'q'\n>").lower()
-   # if start == "guide" or "book" or "guide book" or "guidebook":
-book()
+    start = " "
+    while start != "q":
+        start = input("Where would you like to begin?\nYou may retrieve the Guide Book or begin brewing on your own!\nAlso, feel free to take a break from your alchemical pursuits anytime with 'q'\n>").lower()
+        if start == "q":
+            sys.exit("Good Luck in Your Endeavors!")
+        elif start in ["guide book", "guide", "book", "guidebook", "pick up the book", "pick up the guide book", "pick up the guidebook"]:
+            book()
+        elif start in ["brewpotions", "brewing", "begin brewing", "brew", "brew potions"]:
+            print("\nThis feature is not yet implemented\n")
+        else:
+            print("\nPlease pick up the book or begin brewing.\nIf you would like to exit, select 'q'\n")
+
+main()
